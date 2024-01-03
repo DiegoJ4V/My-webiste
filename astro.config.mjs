392bbef46c19262922cf21c4c0a64f5641b8571e
DiegoJ4V <diegoj4v@gmail.com>
2024-01-03 17:43:56 -0600

    Test base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig, squooshImageService } from 'astro/config';
 
 export default defineConfig({
    site: 'https://diegoj4v.github.io',
+   base: 'My-website',
    service: squooshImageService()
 });
