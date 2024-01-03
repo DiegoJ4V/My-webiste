b96ea3a3bff78b186d6d9faa4bb908ab3b735735
DiegoJ4V <diegoj4v@gmail.com>
2024-01-03 17:58:09 -0600

    Change image and li styles from portfolio card

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig, squooshImageService } from 'astro/config';
 
 export default defineConfig({
    site: 'https://diegoj4v.github.io',
-   base: 'My-website',
+   base: '/My-website',
    service: squooshImageService()
 });
